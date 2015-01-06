a17bbe92b862b1ea11b4bbf6b1f52520815d6967
HoangPN <phamhoang2812@gmail.com>
2015-01-06 16:17:13 +0700

    + create UI Dashboard-Calendar

--- Classes/view/DashboardCalendar/DashBoardCalendarViewController.m
+++ Classes/view/DashboardCalendar/DashBoardCalendarViewController.m
@@ -8,7 +8,25 @@
 
 #import "DashBoardCalendarViewController.h"
 
-@interface DashBoardCalendarViewController ()
+
+#import "JTCalendar.h"
+
+#import "Globals.h"
+
+@interface DashBoardCalendarViewController ()<JTCalendarDataSource>
+{
+    IBOutlet UIView *_calView;
+    IBOutlet UIButton *btnToday;
+    IBOutlet UIButton *btnChangeMode;
+}
+
+
+@property (weak, nonatomic) IBOutlet JTCalendarMenuView *calendarMenuView;
+@property (weak, nonatomic) IBOutlet JTCalendarContentView *calendarContentView;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *calendarContentViewHeight;
+
+@property (strong, nonatomic) JTCalendar *calendar;
 
 @end
 
@@ -17,6 +35,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    /* Prepare the events array */
+    
+    self.calendar = [JTCalendar new];
+    
+    // All modifications on calendarAppearance have to be done before setMenuMonthsView and setContentView
+    // Or you will have to call reloadAppearance
+    {
+        self.calendar.calendarAppearance.calendar.firstWeekday = 2; // Sunday == 1, Saturday == 7
+        self.calendar.calendarAppearance.dayCircleRatio = 9. / 10.;
+        self.calendar.calendarAppearance.ratioContentMenu = 1.;
+    }
+    
+    [self.calendar setMenuMonthsView:self.calendarMenuView];
+    [self.calendar setContentView:self.calendarContentView];
+    [self.calendar setDataSource:self];
+    
+    btnToday.layer.cornerRadius = CORNER_RADIUS_BUTTON;
+    btnToday.layer.masksToBounds = YES;
+    btnChangeMode.layer.cornerRadius = CORNER_RADIUS_BUTTON;
+    btnChangeMode.layer.masksToBounds = YES;
+
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.calendar reloadData]; // Must be call in viewDidAppear
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +80,64 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+-(IBAction) homeBack:(id)sender{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Buttons callback
+
+- (IBAction)didGoTodayTouch
+{
+    [self.calendar setCurrentDate:[NSDate date]];
+}
+
+- (IBAction)didChangeModeTouch
+{
+    self.calendar.calendarAppearance.isWeekMode = !self.calendar.calendarAppearance.isWeekMode;
+    
+    [self transitionExample];
+}
+
+#pragma mark - JTCalendarDataSource
+
+- (BOOL)calendarHaveEvent:(JTCalendar *)calendar date:(NSDate *)date
+{
+    return (rand() % 10) == 1;
+}
+
+- (void)calendarDidDateSelected:(JTCalendar *)calendar date:(NSDate *)date
+{
+    NSLog(@"Date: %@", date);
+}
+
+#pragma mark - Transition examples
+
+- (void)transitionExample
+{
+    CGFloat newHeight = 300;
+    if(self.calendar.calendarAppearance.isWeekMode){
+        newHeight = 75.;
+    }
+    
+    [UIView animateWithDuration:.5
+                     animations:^{
+                         self.calendarContentViewHeight.constant = newHeight;
+                         [self.view layoutIfNeeded];
+                     }];
+    
+    [UIView animateWithDuration:.25
+                     animations:^{
+                         self.calendarContentView.layer.opacity = 0;
+                     }
+                     completion:^(BOOL finished) {
+                         [self.calendar reloadAppearance];
+                         
+                         [UIView animateWithDuration:.25
+                                          animations:^{
+                                              self.calendarContentView.layer.opacity = 1;
+                                          }];
+                     }];
+}
 
 @end

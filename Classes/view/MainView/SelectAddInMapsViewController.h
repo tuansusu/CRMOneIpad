1d43765eb054096b8a85ad8bd056da8fff6c3ab7
Luong Hoang <luongit91@gmail.com>
2015-01-21 00:40:21 +0700

    ..

--- Classes/view/MainView/SelectAddInMapsViewController.h
+++ Classes/view/MainView/SelectAddInMapsViewController.h
@@ -7,11 +7,15 @@
 //
 
 #import "BaseViewController.h"
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface SelectAddInMapsViewController : BaseViewController<UISearchBarDelegate, UISearchDisplayDelegate>
 
 @property (strong, nonatomic) IBOutlet UIView *view;
 
 @property (weak, nonatomic) IBOutlet UISearchBar *txtSearchBar;
+@property (weak, nonatomic) IBOutlet MKMapView *mkMapview;
 
 @end
+   

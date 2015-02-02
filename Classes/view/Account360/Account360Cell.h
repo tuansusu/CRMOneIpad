c2ab549ca7a8e86f0e83a81e08bc7834184d9b31
tuansusu <tuansusu@gmail.com>
2015-02-02 23:42:48 +0700

    1

--- Classes/view/Account360/Account360Cell.h
+++ Classes/view/Account360/Account360Cell.h
@@ -30,6 +30,9 @@
 @property (weak, nonatomic) id <Account360CellDelegate> delegate;
 @property (nonatomic, strong) NSDictionary *dicData;
 
+@property (weak, nonatomic) IBOutlet UIImageView *imgAvatar;
+
+
 @property (weak, nonatomic) IBOutlet UILabel *lbName;
 @property (weak, nonatomic) IBOutlet UILabel *lbPhone;
 

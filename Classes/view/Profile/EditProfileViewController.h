7fc34b58ab778327276d97e39fdd0cf1200f146b
tuansusu <tuansusu@gmail.com>
2015-04-20 22:24:56 +0700

    commit tam

---
+++ Classes/view/Profile/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  OfficeOneMB
+//
+//  Created by viettel on 4/20/15.
+//
+//
+
+#import "BaseViewController.h"
+
+@interface EditProfileViewController : BaseViewController
+
+@end
